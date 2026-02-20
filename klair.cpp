b68d77194d7130d93588e8a14e53e5f6bec34ed1
klairt <klair.tulip.studente@itispaleocapa.it>
2026-02-20 10:31:51 +0100

    funzioni

--- klair.cpp
+++ klair.cpp
@@ -9,13 +9,36 @@ struct prodotto
 	float prezzo;
 };
 	
-	
+void add(prodotto p, prodotto v[], int d)
+{
+	//aggiunge un prodotto
+}
 
+prodotto view(prodotto v[], int d)
+{
+	//visualizza un prodotto
+}
+
+int search(prodotto p, prodotto v[], int d)
+{
+	//restituisce la posizione del prodotto
+}
+
+bool canc(prodotto p, prodotto v[], int d)
+{
+	//restituisce un booleano
+}
+
+bool modify(prodotto p, prodotto v[], int d)
+{
+	//restituisce un booleano
+}
 
 int main(int argc, char** argv) {
 	
 	prodotto p;
 	prodotto supermercato[100];
+	int dim=sizeof(supermercato)/sizeof(supermercato[0]);
 	
 	
 	return 0;
