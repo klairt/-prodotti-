3682f5467f6995ebac0c0f882e6e36894117e675
klairt <klair.tulip.studente@itispaleocapa.it>
2026-02-20 10:40:09 +0100

    menù

--- klair.cpp
+++ klair.cpp
@@ -2,13 +2,16 @@
 #include <string>
 using namespace std;
 
+//struct
 struct prodotto
 {
 	string nome_prod;
 	string categoria;
 	float prezzo;
 };
-	
+
+
+//funzioni	
 void add(prodotto p, prodotto v[], int d)
 {
 	//aggiunge un prodotto
@@ -24,12 +27,12 @@ int search(prodotto p, prodotto v[], int d)
 	//restituisce la posizione del prodotto
 }
 
-bool canc(prodotto p, prodotto v[], int d)
+bool erase(prodotto p, prodotto v[], int d)
 {
 	//restituisce un booleano
 }
 
-bool modify(prodotto p, prodotto v[], int d)
+bool edit(prodotto p, prodotto v[], int d)
 {
 	//restituisce un booleano
 }
@@ -39,7 +42,42 @@ int main(int argc, char** argv) {
 	prodotto p;
 	prodotto supermercato[100];
 	int dim=sizeof(supermercato)/sizeof(supermercato[0]);
+	int opzione=0;
 	
-	
+	 do
+    {
+        cout<<"0 - Ferma programma\n";
+        cout<<"1 - Inserimento\n";
+        cout<<"2 - Cancellazione\n";
+        cout<<"3 - Modifica\n";
+        cout<<"4 - Visualizzazione\n";
+        cout<<"5 - Ricerca\n";
+        cout<<"Scegli opzione: ";
+
+        cin>>opzione;
+
+        switch(opzione)
+        {
+            case 0: break;
+            case 1: //Inserimento
+                add(p,supermercato,dim);
+                break;
+            case 2://Cancellazione
+                erase(p,supermercato,dim);
+                break;
+            case 3://Modifica
+                edit(p,supermercato,dim);
+                break;
+            case 4://Visualizzazione
+                view(supermercato,dim);
+                break;
+            case 5://Ricerca
+                search(p,supermercato,dim);
+                break;
+
+            default: //Nel caso in cui l'utente sceglie un numero diverso
+            cout<<"\nOpzione non valida.\n\n";
+        }
+
 	return 0;
 }
