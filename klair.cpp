0e4168d96fa2f8f11f7d42e2fb20df391488b10f
klairt <klair.tulip.studente@itispaleocapa.it>
2026-02-20 10:23:33 +0100

    struttura

--- klair.cpp
+++ klair.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-
+struct prodotto
+{
+	string nome_prod;
+	string categoria;
+	float prezzo;
+};
+	
+	
 
 
 int main(int argc, char** argv) {
-
+	
+	prodotto p;
+	prodotto supermercato[100];
 	
 	
 	return 0;
